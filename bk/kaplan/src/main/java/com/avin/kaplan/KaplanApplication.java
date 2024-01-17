2941d939dd185c907f268bd9c3de228f3df056aa
e.sadeghi <e.sadeghi@fwutech.com>
2024-01-17 14:20:28 -0500

    first commit

---
+++ bk/kaplan/src/main/java/com/avin/kaplan/KaplanApplication.java
@@ -0,0 +1,13 @@
+package com.avin.kaplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaplanApplication.class, args);
+	}
+
+}
---
+++ bk/kaplan/src/test/java/com/avin/kaplan/KaplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avin.kaplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
